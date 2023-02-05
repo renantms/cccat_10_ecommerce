e8bd15d120fcc1038fa7eb0b4cec24326658e686
Renan Scolari <renantms@gmail.com>
2023-02-05 12:16:19 -0300

    Começa criação de testes de integração

---
+++ src/test/java/br/com/cccat10/ecommerce/CreateOrderTest.java
@@ -0,0 +1,14 @@
+package br.com.cccat10.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CreateOrderTest {
+
+    @Test
+    void shouldCreateOrder() {
+
+    }
+
+}
